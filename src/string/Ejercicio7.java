0a038896c629b8a1cbe6fe7f0266c3963b14f802
rubenruizc <ruben.ruiz@iesnervion.es>
2024-02-16 02:23:00 +1300

    Ejercicio 7-8-9 t4 String

--- src/string/Ejercicio7.java
+++ src/string/Ejercicio7.java
@@ -5,10 +5,67 @@ import java.util.Scanner;
 public class Ejercicio7 {
 
 	public static void main(String[] args) {
-		String palabra;
+
+		// Variable donde guardaremos la frase del usuario
 		String frase;
-		Scanner sc = new Scanner (System.in);
 
-	}
+		// Variable donde guardaremos la palabra del usuario
+		String palabra;
 
-}
+		// Scanner para leer del teclado
+		Scanner sc = new Scanner(System.in);
+
+		// Le preguntamos al usuario la frase y la leemos
+		System.out.println("Escriba una frase: ");
+		frase = sc.nextLine();
+
+		// Le preguntamos al usuario la palabra que quiere buscar y la leemos
+		System.out.println("Escriba la palabra que quieras buscar en la frase: ");
+		palabra = sc.next();
+
+		if (contarPalabra(frase, palabra) > 1) {
+			// Llamando a la función sabemos la cantidad de veces que aparece la palabra
+			// deseada
+			System.out.println(
+					"La palabra \"" + palabra + "\" aparece " + contarPalabra(frase, palabra) + " veces en la frase.");
+		
+		}else if (contarPalabra(frase, palabra) == 1) {
+			System.out.println(
+					"La palabra \"" + palabra + "\" aparece " + contarPalabra(frase, palabra) + " vez en la frase.");
+		} else {
+			System.out.println(
+					"La palabra \"" + palabra + "\" no aparece en la frase");
+		}
+
+		// Cierre del Scanner
+		sc.close();
+		
+	} // Cierre del main
+
+	// Función para contar las veces que sale la palabra que buscamos
+	public static int contarPalabra(String frase, String palabra) {
+		// Variable donde guardaremos la cantidad de veces que esta la palabra en la
+		// frase
+		int contador = 0;
+
+		// Convertir la frase y la palabra a minúsculas para que no influyan las
+		// mayúsculas
+		frase = frase.toLowerCase();
+		palabra = palabra.toLowerCase();
+
+		// Dividir la frase en palabras
+		String[] palabras = frase.split(" ");
+
+		// Recorrer cada palabra y comparar con la palabra buscada
+		for (String p : palabras) {
+			if (p.equals(palabra)) {
+				contador++;
+			}
+		}
+
+		// Devolvemos la cantidad de veces que se ha encontrado la palabra
+		return contador;
+
+	} // Cierre de la función
+
+} // Cierre de la clase
---
+++ src/string/Ejercicio8.java
@@ -0,0 +1,53 @@
+package string;
+
+import java.util.Scanner;
+
+public class Ejercicio8 {
+
+	public static void main(String[] args) {
+
+		// Variable donde guardaremos la frase del usuario
+		String frase;
+
+		// Scanner para leer del teclado
+		Scanner sc = new Scanner(System.in);
+
+		// Le pedimos al usuario la frase y la leemos
+		System.out.println("Escriba una frase:  (SIN TILDES)");
+		frase = sc.nextLine();
+
+		// Llamamos a la función e imprimimos en base al resultado
+		if (esPalíndroma(frase.replace(" ", ""))) {
+			System.out.println("Su frase es palíndroma");
+		} else {
+			System.out.println("Su frase no es palíndroma");
+		}
+		// Cierre del Scanner
+		sc.close();
+
+	} // Cierre del main
+
+	// Función que comprueba si la frase añadida es Palíndroma
+	public static boolean esPalíndroma(String fraseNormal) {
+
+		// Variable donde guardaremos el resultado
+		boolean es = false;
+
+		// Variable donde guardaremos la frase del usuario pero invertida
+		String fraseInvertida;
+
+		// Llamamos a la función del ejercicio 5 para leer la frase alreves
+		fraseInvertida = Ejercicio5.fraseInvertida(fraseNormal);
+
+		// Si la frase alreves y la frase normal son iguales cambiamos el resultado a
+		// true
+		if (fraseInvertida.equalsIgnoreCase(fraseNormal)) {
+			es = true;
+		}
+
+		// Devolvemos si la frase es o no Palíndroma
+		return es;
+
+	} // Cierre de la función
+
+} // Cierre de la clase
